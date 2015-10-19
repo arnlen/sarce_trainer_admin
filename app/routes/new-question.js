18d1fb710d18f1b4ea1840d41e5e4e429a81ac25
Arnaud Lenglet <arlenglet@gmail.com>
2015-10-19 10:28:43 +0200

    feat(view): add first questions index view

--- app/routes/new-question.js
+++ app/routes/new-question.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.createRecord('question', { body: "hello" })
+    return this.store.createRecord('question', { body: "hello" });
   }
 });
