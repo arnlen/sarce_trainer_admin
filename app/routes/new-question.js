b78e3cd0fa5302b54875c30e7d8fbd5d3ccb1376
Arnaud Lenglet <arlenglet@gmail.com>
2015-12-01 23:48:25 +0100

    feat(question): improve new question form

--- app/routes/new-question.js
+++ app/routes/new-question.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.createRecord('question', { body: "hello" });
+    return this.store.createRecord('question');
   },
   afterModel() {
     this.controllerFor('application').set('pageTitle', "Nouvelle question");
